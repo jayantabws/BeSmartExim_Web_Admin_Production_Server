7bd1583f33e3f328313c2796b9710252d7f189b0
jayantabws <61349408+jayantabws@users.noreply.github.com>
2025-07-29 11:31:53 +0530

    Update AppConstant.js

--- src/components/shared/AppConstant.js
+++ src/components/shared/AppConstant.js
@@ -16,12 +16,17 @@
 // export const MASTER_API_URL = "http://38.114.123.217:8084/"; // Master Data API Base URL
 
 
-export const API_URL = "http://97.74.85.75:8081/"; // API Base URL
-export const API_USER_URL = "http://97.74.85.75:8082/"; //User service url
-export const API_ACT_URL = "http://97.74.85.75:8083/"; //User Activity API URL
-export const MASTER_API_URL = "http://97.74.85.75:8084/"; // Master Data API Base URL
+// export const API_URL = "http://97.74.85.75:8081/"; // API Base URL
+// export const API_USER_URL = "http://97.74.85.75:8082/"; //User service url
+// export const API_ACT_URL = "http://97.74.85.75:8083/"; //User Activity API URL
+// export const MASTER_API_URL = "http://97.74.85.75:8084/"; // Master Data API Base URL
 
 // export const API_URL = "https://api.eximwatch.com/"; // API Base URL
 // export const API_USER_URL = "https://api.eximwatch.com/"; //User service url
 // export const API_ACT_URL = "https://api.eximwatch.com/"; //User Activity API URL
 // export const MASTER_API_URL = "https://api.eximwatch.com/"; // Master Data API Base URL
+
+export const API_URL = "http://188.40.197.10:30081/"; // API Base URL
+export const API_USER_URL = "http://188.40.197.10:30082/"; //User service url
+export const API_ACT_URL = "http://188.40.197.10:30083/"; //User Activity API URL
+export const MASTER_API_URL = "http://188.40.197.10:30084/"; // Master Data API Base URL
